e4e6934eff4566f1ce6310a846a3e67b657c604d
Bram Kuijper <bramkuijper@gmail.com>
2016-09-17 23:01:41 +0100

    set the stage for doing the iterations

--- src/numerical/1player/generate_iters.py
+++ src/numerical/1player/generate_iters.py
@@ -6,45 +6,42 @@ from numpy import *
 
 # frequency of envt 2
 #freq_patch_2 = list(arange(0.01,0.99,0.02))
-freq_patch_2 = list(arange(0.01,0.99,0.1))
+freq_patch_2 = list(arange(0.01,0.99,0.02))
 
 # avarage switch rate
 #sbar = list(arange(-1.5, 0.5, 2.0/50))
-sbar = list(arange(-1.5, 0.5, 2.0/10))
-k = [ 0.25 ]#[ 1.0, 2.0, 0.1 ]
-d = [ 0.1 ]
+sbar = list(arange(-1.5, 0.5, 2.0/50))
+d = [ 0.1, 0.5, 1.0 ]
 
 exe = "./numsolve"
 
 max_iter = 1000000
 
 # initial values for patch freqs and relatedness coeffs
-fval_init = " ".join([ str(1.0/4) for xi in range(0,4) ])
+fval_init = " ".join([ str(1.0/8) for xi in range(0,8) ])
 
 # initial values for the reproductive values
-vval_init = " ".join([ str(1.0) for xi in range(0,4) ])
+vval_init = " ".join([ str(1.0) for xi in range(0,8) ])
 
 ctr = 0
 
 for f2 in freq_patch_2:
     for sbar_i in sbar:
-        for k_i in k:
-            for d_i in d:
-                s2 = sqrt(((1.0-f2)/f2) * 10**(2*sbar_i))
-                s1 = 10**(2*sbar_i) / s2
-                print("echo " + str(ctr))
-                print(exe + " " + str(max_iter) + " " 
-                        + str(2) + " " 
-                        + str(1) + " " 
-                        + str(d_i) + " " 
-                        + str(s1) + " " 
-                        + str(s2) + " " 
-                        + str(k_i) + " " 
-                        + str(0.5) + " " 
-                        + str(0.5) + " " 
-                        + str(1.0) + " " 
-                        + str(1.0) + " " 
-                        + fval_init + " "
-                        + vval_init + " ")
-                ctr+=1
-            
+        for d_i in d:
+            s2 = sqrt(((1.0-f2)/f2) * 10**(2*sbar_i))
+            s1 = 10**(2*sbar_i) / s2
+            print("echo " + str(ctr))
+            print(exe + " " + str(max_iter) + " " 
+                    + str(2) + " " 
+                    + str(1) + " " 
+                    + str(d_i) + " " 
+                    + str(s1) + " " 
+                    + str(s2) + " " 
+                    + str(0.5) + " " 
+                    + str(0.5) + " " 
+                    + str(0.5) + " " 
+                    + str(0.5) + " " 
+                    + fval_init + " "
+                    + vval_init + " ")
+            ctr+=1
+        
